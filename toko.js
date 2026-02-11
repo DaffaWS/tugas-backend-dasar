eb2e7121077226c6cf05a354e60106e75562179a
daffa <daffa.w.s1999@gmail.com>
2026-02-11 16:52:13 +0700

    Selesai mengerjakan latihan json bagian toko.js

---
+++ toko.js
@@ -0,0 +1,15 @@
+//File: toko.js
+console.log("=== APLIKASI KASIR SEDERHANA ===");
+
+// 1.Data Barang
+let namaBarang = "Apel Fuji";
+let hargaSatuan = 5000;
+let jumlahBeli = 12;
+
+// 2.Proses Hiung
+let totalHarga = hargaSatuan * jumlahBeli;
+
+// 3.Tampilan Struk
+console.log("Barang: " + namaBarang);
+console.log("Harga: Rp " + hargaSatuan);
+comsloe.log("Jumlah " + jumlahBeli + " buah")
