315c3323ad3071b65c7bc494b357bf1a73d6e7c9
DaffaWS <daffa.w.s1999@gmail.com>
2026-02-12 18:24:58 +0700

    Sudah berhasil menyelesaikan tugas bagian 1 bipdata.js

---
+++ biodata.js
@@ -0,0 +1,13 @@
+//biodata.js
+
+let nama = "Daffa Wahyu S";
+let kelas = "11";
+let jurusan = "TKJ A";
+let hobi = "Mempelajari sesuatu yang baru";
+
+console.log("==== BIODATA SISWA ====") ;
+console.log("Nama: " + nama);
+console.log("Kelaa: " + kelas + " " + jurusan);
+console.log("hobi: " + hobi );
+console.log("=== Biodata Selesai ===");
+// --- END OF FILE ---);
