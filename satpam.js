cf4ecb969707d317d58dcf436bb478c618a3709a
DaffaWS <daffa.w.s1999@gmail.com>
2026-02-12 17:46:09 +0700

    Sudah selesai mempelajari json bagian satpam.js

--- satpam.js
+++ satpam.js
@@ -6,3 +6,4 @@ console.log("=== Pengecekan Masuk Bioskop ===");
 
 //logika: harus berumur minimal 17 tahun dan membawa KTP
 if (umurPenggunjung >= 17 && bawaKTP === true) /* tanda >= berarti lebih dari atau sama dengan dan && berarti dan. sedangkan tanda === berfungsi untuk membandingkan nilai dan tipe data dan true berarti benar */ { console.log("Silahkan Masuk, selamat menonton!"); } else if (umurPenggunjung >= 17 && bawaKTP === false) { console.log("Maaf, umur anda sudah cukup tapi Anda lupa membawa ktp "); } else { console.log("Dilarang masuk, belum cukup umur"); }
+// --- END OF FILE ---
